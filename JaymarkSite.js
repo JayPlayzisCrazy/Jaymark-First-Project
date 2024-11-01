d202b67e6de0213c4cc79bcf0293c4702c2b09d2
JayPlayzisCrazy <jaymark14.mutants@gmail.com>
2024-11-02 01:33:54 +0800

    Add files via upload

---
+++ JaymarkSite.js
@@ -0,0 +1,5 @@
+// Toggle function for menu
+function toggleMenu() {
+    document.querySelector('.menu-icon').classList.toggle('active');
+    document.getElementById('menuList').classList.toggle('active');
+  }
